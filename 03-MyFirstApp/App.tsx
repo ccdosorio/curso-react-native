2de22322b6be21f9b5a9be81d888af502542791f
Christian Osorio <ccdosorio11@gmail.com>
2024-04-11 00:02:51 -0600

    Fin seccion 4: MyFirstApp

---
+++ 03-MyFirstApp/App.tsx
@@ -0,0 +1,22 @@
+import {SafeAreaView} from 'react-native';
+// import {CounterScreen} from './src/presentation/screens/CounterScreen';
+import {PaperProvider} from 'react-native-paper';
+import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
+// import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
+
+import IonIcon from 'react-native-vector-icons/Ionicons';
+
+export const App = () => {
+  return (
+    <PaperProvider
+      settings={{
+        icon: props => <IonIcon {...props} />,
+      }}>
+      <SafeAreaView style={{flex: 1}}>
+        {/* <HelloWorldScreen name="Christian Osorio" /> */}
+        {/* <CounterScreen /> */}
+        <CounterM3Screen />
+      </SafeAreaView>
+    </PaperProvider>
+  );
+};
