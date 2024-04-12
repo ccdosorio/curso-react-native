ca2c491be8e0d1a56f17debea6732ae14734c147
Christian Osorio <ccdosorio11@gmail.com>
2024-04-12 15:09:27 -0600

    useSafeAreaInsets

--- 04-NavigationApp/src/presentation/screens/profile/ProfileScreen.tsx
+++ 04-NavigationApp/src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,9 +1,20 @@
 import {Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {PrimaryBotton} from '../../components/shared/PrimaryBotton';
+import {DrawerActions, useNavigation} from '@react-navigation/native';
 
 export const ProfileScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const navigation = useNavigation();
+
   return (
-    <View>
+    <View style={{flex: 1, paddingHorizontal: 20, marginTop: top + 20}}>
       <Text>ProfileScreen</Text>
+
+      <PrimaryBotton
+        label="Abrir menu"
+        onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}
+      />
     </View>
   );
 };
