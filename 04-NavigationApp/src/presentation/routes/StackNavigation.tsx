b1fa1868ee964a4e05ab7c0ed283d2f9d0856a66
Christian Osorio <ccdosorio11@gmail.com>
2024-04-12 14:12:38 -0600

    Enviar argumentos entre pantallas, useEffect para nombrar header pantalla

--- 04-NavigationApp/src/presentation/routes/StackNavigation.tsx
+++ 04-NavigationApp/src/presentation/routes/StackNavigation.tsx
@@ -1,9 +1,16 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../screens/home/HomeScreen';
-import {ProductScreen} from '../screens/products/ProductScreen';
+import {ProductsScreen} from '../screens/products/ProductsScreen';
 import {SettingsScreen} from '../screens/settings/SettingsScreen';
+import {ProductScreen} from '../screens/products/ProductScreen';
 
-const Stack = createStackNavigator();
+export type RootStackParams = {
+  Home: undefined;
+  Product: {id: number; name: string};
+  Products: undefined;
+  Settings: undefined;
+};
+const Stack = createStackNavigator<RootStackParams>();
 
 export const StackNavigator = () => {
   return (
@@ -13,7 +20,8 @@ export const StackNavigator = () => {
         headerStyle: {elevation: 0, shadowColor: 'transparent'},
       }}>
       <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Products" component={ProductScreen} />
+      <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
--- 04-NavigationApp/src/presentation/screens/products/ProductsScreen.tsx
+++ 04-NavigationApp/src/presentation/screens/products/ProductsScreen.tsx
@@ -1,9 +1,44 @@
 import {Text, View} from 'react-native';
+import {globalStyles} from '../../../config/theme/theme';
+import {FlatList} from 'react-native-gesture-handler';
+import {PrimaryBotton} from '../../components/shared/PrimaryBotton';
+import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {type RootStackParams} from '../../routes/StackNavigation';
+
+const products = [
+  {id: 1, name: 'Producto 1'},
+  {id: 2, name: 'Producto 2'},
+  {id: 3, name: 'Producto 3'},
+  {id: 4, name: 'Producto 4'},
+  {id: 5, name: 'Producto 5'},
+  {id: 6, name: 'Producto 6'},
+];
 
 export const ProductsScreen = () => {
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
   return (
-    <View>
-      <Text>ProductsScreen</Text>
+    <View style={globalStyles.container}>
+      <Text style={{marginBottom: 10, fontSize: 30}}>Productos</Text>
+
+      <FlatList
+        data={products}
+        renderItem={({item}) => (
+          <PrimaryBotton
+            label={item.name}
+            onPress={() =>
+              navigation.navigate('Product', {id: item.id, name: item.name})
+            }
+          />
+        )}
+      />
+
+      <Text style={{marginBottom: 10, fontSize: 30}}>Ajustes</Text>
+
+      <PrimaryBotton
+        label="Ajustes"
+        onPress={() => navigation.navigate('Settings')}
+      />
     </View>
   );
 };
