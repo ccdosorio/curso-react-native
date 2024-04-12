c53ca830d5d40a45ce18f9e5ba5fd48c8043d5a6
Christian Osorio <ccdosorio11@gmail.com>
2024-04-11 22:12:51 -0600

    Align self

--- 03-MyFirstApp/App.tsx
+++ 03-MyFirstApp/App.tsx
@@ -1,14 +1,15 @@
 import {SafeAreaView} from 'react-native';
 import {PaperProvider} from 'react-native-paper';
+import React from 'react';
+import IonIcon from 'react-native-vector-icons/Ionicons';
 // import {CounterScreen} from './src/presentation/screens/CounterScreen';
 // import {CounterM3Screen} from './src/presentation/screens/CounterM3Screen';
 // import {HelloWorldScreen} from './src/presentation/screens/HelloWorldScreen';
 // import {BoxObjectModelScreen} from './src/presentation/screens/BoxObjectModelScreen';
-
-import IonIcon from 'react-native-vector-icons/Ionicons';
-import {DimensionScreen} from './src/presentation/screens/DimensionScreen';
-import {PositonScreen} from './src/presentation/screens/PositonScreen';
-import {FlexScreen} from './src/presentation/screens/FlexScreen';
+// import {DimensionScreen} from './src/presentation/screens/DimensionScreen';
+// import {PositonScreen} from './src/presentation/screens/PositonScreen';
+// import {FlexScreen} from './src/presentation/screens/FlexScreen';
+import {FlexDirectionScreen} from './src/presentation/screens/FlexDirectionScreen';
 
 export const App = () => {
   return (
@@ -23,7 +24,8 @@ export const App = () => {
         {/* <BoxObjectModelScreen /> */}
         {/* <DimensionScreen /> */}
         {/* <PositonScreen /> */}
-        <FlexScreen />
+        {/* <FlexScreen /> */}
+        <FlexDirectionScreen />
       </SafeAreaView>
     </PaperProvider>
   );
---
+++ 03-MyFirstApp/src/presentation/screens/FlexDirectionScreen.tsx
@@ -0,0 +1,42 @@
+import {StyleSheet, View} from 'react-native';
+
+export const FlexDirectionScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={[styles.box, styles.box1]} />
+      <View style={[styles.box, styles.box2]} />
+      <View style={[styles.box, styles.box3]} />
+      <View style={[styles.box, styles.box4]} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#cfcaca',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    flexDirection: 'row',
+  },
+  box: {
+    // flex: 1,
+    width: 100,
+    height: 100,
+  },
+  box1: {
+    backgroundColor: '#5856D6',
+  },
+  box2: {
+    backgroundColor: '#3f3cc4',
+    alignSelf: 'flex-end',
+  },
+  box3: {
+    backgroundColor: '#201e87',
+    // alignSelf: 'center',
+  },
+  box4: {
+    backgroundColor: '#09084f',
+    // alignSelf: 'center',
+  },
+});
