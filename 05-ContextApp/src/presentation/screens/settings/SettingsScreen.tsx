7650e6fcf70fb8e7a15515ff6e2e9857b9d414d5
Christian Osorio <ccdosorio11@gmail.com>
2024-04-13 10:48:34 -0600

    create counter with zustand

--- 05-ContextApp/src/presentation/screens/settings/SettingsScreen.tsx
+++ 05-ContextApp/src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,35 @@
-import {Text, View} from 'react-native';
+import {Pressable, Text, View} from 'react-native';
 import {globalStyles} from '../../../config';
+import {useCounterStore} from '../../store/counter-store';
+import {useNavigation} from '@react-navigation/native';
+import {useEffect} from 'react';
 
 export const SettingsScreen = () => {
+  const counter = useCounterStore(state => state.counter);
+  const incrementBy = useCounterStore(state => state.incrementBy);
+
+  const navigaton = useNavigation();
+
+  useEffect(() => {
+    navigaton.setOptions({
+      title: `Contador: ${counter}`,
+    });
+  }, [counter]);
+
   return (
     <View style={globalStyles.container}>
-      <Text>SettingsScreen</Text>
+      <Text style={globalStyles.title}>Count: {counter}</Text>
+
+      <Pressable
+        style={globalStyles.primaryButton}
+        onPress={() => incrementBy(+1)}>
+        <Text>+1</Text>
+      </Pressable>
+      <Pressable
+        style={globalStyles.primaryButton}
+        onPress={() => incrementBy(-1)}>
+        <Text>-1</Text>
+      </Pressable>
     </View>
   );
 };
