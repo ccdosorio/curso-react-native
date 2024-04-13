fc4328b754a1160b16da15275b0da2f5a0df9cba
Christian Osorio <ccdosorio11@gmail.com>
2024-04-12 23:39:31 -0600

    configuration icons

--- 04-NavigationApp/src/presentation/screens/tabs/Tab1Screen.tsx
+++ 04-NavigationApp/src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,11 +1,13 @@
 import {Text, View} from 'react-native';
 import {HamburgerMenu} from '../../components/shared/HamburgerMenu';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const Tab1Screen = () => {
   return (
     <View>
       <HamburgerMenu />
       <Text>Tab1Screen</Text>
+      <Icon name="rocket-outline" size={100}/>
     </View>
   );
 };
