f311b36733ceeb4b16e50277a93aea5c325e14c2
Christian Osorio <ccdosorio11@gmail.com>
2024-04-14 23:14:26 -0600

    Add use case get movie

---
+++ 06-moviesApp/src/presentation/hooks/useMovie.tsx
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+import * as UseCases from '../../core/use-cases';
+import {movieDBFetcher} from '../../config/adapters/movieDB.adapter';
+import {FullMovie} from '../../core/entities/movie.entity';
+
+export const useMovie = (movieId: number) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState<FullMovie>();
+  useEffect(() => {
+    loadMovie();
+  }, [movieId]);
+
+  const loadMovie = async () => {
+    setIsLoading(true);
+    const fullMoviePromise = UseCases.getMovieByIdUseCase(
+      movieDBFetcher,
+      movieId,
+    );
+    const [fullMovie] = await Promise.all([fullMoviePromise]);
+    console.log({fullMovie});
+
+    setMovie(fullMovie);
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    movie,
+  };
+};
--- 06-moviesApp/src/presentation/screens/details/DetailsScreen.tsx
+++ 06-moviesApp/src/presentation/screens/details/DetailsScreen.tsx
@@ -1,9 +1,38 @@
+import {StackScreenProps} from '@react-navigation/stack';
 import {Text, View} from 'react-native';
+import {RootStackParams} from '../../navigation/StackNavigator';
 
-export const DetailsScreen = () => {
+import {ScrollView} from 'react-native-gesture-handler';
+import {useMovie} from '../../hooks/useMovie';
+// import { FullScreenLoader } from '../../components/loaders/FullScreenLoader';
+
+interface Props extends StackScreenProps<RootStackParams, 'Details'> {}
+
+export const DetailsScreen = ({route}: Props) => {
+  const {movieId} = route.params;
+  const {} = useMovie(movieId);
+  // const { isLoading, movie, cast = [] } = useMovie( movieId );
+
+  // if (isLoading) {
+  //   return <FullScreenLoader />
+  // }
+
   return (
     <View>
-      <Text>DetailsScreen</Text>
+      <Text>Id: {movieId}</Text>
     </View>
+    // <ScrollView>
+
+    //   {/* Header */}
+    //   <MovieHeader
+    //     originalTitle={ movie!.originalTitle}
+    //     title={ movie!.title}
+    //     poster={ movie!.poster }
+    //   />
+
+    //   {/* Details */}
+    //   <MovieDetails movie={ movie! } cast={cast} />
+
+    // </ScrollView>
   );
 };
